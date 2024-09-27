78f2030b884b364e677d1a1cd9f6b69daad44d27
Eunchurn Park <eunchurn.park@gmail.com>
2024-09-27 10:06:12 +0900

    chore(theme): updated

--- src/components/Nav/NavCategoryCollapsible.tsx
+++ src/components/Nav/NavCategoryCollapsible.tsx
@@ -91,7 +91,10 @@ function NavItem({
       {...props}
       className={cn(
         'block cursor-pointer rounded-r-xl p-[--NavItem-pad] pl-[--rgrid-m] pr-[calc(2*var(--NavItem-pad)+var(--arrow-size))]',
-        active ? 'interactive-bg-primary-container' : 'interactive-bg-surface',
+        active
+          ? 'interactive-bg-primary-container'
+          : // 'interactive-bg-surface',
+            'bg-white',
         className,
       )}
     >
