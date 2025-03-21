81cfeee4dbca3202e9f7c0a767ab3fab5cdf1e66
Eunchurn Park <eunchurn.park@gmail.com>
2025-03-21 09:33:18 +0900

    fix(pdf serverless): splitted

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -1,10 +1,29 @@
-import puppeteer from 'puppeteer'
+import chromium from '@sparticuz/chromium'
+import puppeteer, { type Browser } from 'puppeteer'
+import puppeteerCore, { type Browser as BrowserCore } from 'puppeteer-core'
+
+chromium.setGraphicsMode = false
 
 export async function POST(req: Request) {
   try {
-    const browser = await puppeteer.launch({
-      headless: 'shell', // headless 모드 사용
-    })
+    let browser: Browser | BrowserCore | null = null
+    if (process.env.NODE_ENV === 'production') {
+      browser = await puppeteerCore.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath(),
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless,
+        // headless: 'shell', // headless 모드 사용
+      })
+    } else if (process.env.NODE_ENV === 'development') {
+      browser = await puppeteer.launch({
+        headless: 'shell',
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      })
+    }
+    if (!browser) {
+      throw new Error('브라우저 생성 실패')
+    }
     const page = await browser.newPage()
     const url = new URL(req.url)
     const { slug } = (await req.json()) as { slug: string[] }
