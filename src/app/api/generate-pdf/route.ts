3d1c1880b2dd14137b1888ac39818fffff720956
Eunchurn Park <eunchurn.park@gmail.com>
2025-04-05 19:52:49 +0900

    fix(pdf): rolledback

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -11,11 +11,10 @@ export async function POST(req: Request) {
     let browser: Browser | BrowserCore | null = null
     if (process.env.NODE_ENV === 'production') {
       browser = await puppeteerCore.launch({
-        // args: chromium.args,
+        args: chromium.args,
         executablePath: await chromium.executablePath(),
-        // defaultViewport: chromium.defaultViewport,
+        defaultViewport: chromium.defaultViewport,
         headless: chromium.headless,
-        // headless: 'shell', // headless 모드 사용
       })
     } else {
       browser = await puppeteer.launch({
