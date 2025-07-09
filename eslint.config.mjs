a81d053f61c01f43e3dd108d13c7bdfac4e03e39
Eunchurn Park <eunchurn.park@gmail.com>
2025-07-09 21:13:53 +0900

    chore(web-agent): updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,15 @@
-export default {
-  extends: 'next',
-  rules: {
-    'react/jsx-no-target-blank': 0,
-    'import/no-anonymous-default-export': 0,
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+export default [
+  ...compat.extends('next'),
+  {
+    rules: {
+      'react/jsx-no-target-blank': 0,
+      'import/no-anonymous-default-export': 0,
+    },
   },
-}
+]
