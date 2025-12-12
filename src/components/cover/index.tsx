fba4a76ea51ade29e49a207819204162ce491ff3
sooki <shnam@onycom.com>
2025-12-12 09:21:19 +0900

    Design: 커버 이미지 교체 및 재배치

--- src/components/cover/index.tsx
+++ src/components/cover/index.tsx
@@ -9,15 +9,11 @@ interface CoverProps {
 }
 
 interface Assets {
-  subtitle: string
   background: string
-  clipBackground: string
   onycom: string
   imqa: string
   q: string
   x: string
-  clip: string
-  clipOffset: string
   typo: string
   typoWidth: string
 }
@@ -26,125 +22,85 @@ const getAssets = (kind: CoverProps['kind']): Assets => {
   switch (kind) {
     case 'android':
       return {
-        subtitle: '설치 가이드',
-        background: 'bg-[url(/cover/bg-sdk.svg)]',
-        clipBackground: 'bg-[url(/cover/bg-sdk-clip.svg)]',
+        background: 'bg-[url(/cover/bg-android-sdk.svg)]',
         onycom: '/cover/onycom.svg',
         imqa: '/cover/imqa.svg',
         q: '/cover/q.svg',
         x: '/cover/x.svg',
-        clip: '/cover/android-clip.svg',
-        clipOffset: '',
         typo: '/cover/android-sdk-typo.svg',
-        typoWidth: 'w-[60%]',
+        typoWidth: 'w-[80%]',
       }
     case 'ios':
       return {
-        subtitle: '설치 가이드',
-        background: 'bg-[url(/cover/bg-sdk.svg)]',
-        clipBackground: 'bg-[url(/cover/bg-sdk-clip.svg)]',
+        background: 'bg-[url(/cover/bg-ios-sdk.svg)]',
         onycom: '/cover/onycom.svg',
         imqa: '/cover/imqa.svg',
         q: '/cover/q.svg',
         x: '/cover/x.svg',
-        clip: '/cover/ios-sdk-clip.svg',
-        clipOffset: '',
         typo: '/cover/ios-sdk-typo.svg',
-        typoWidth: 'w-[60%]',
+        typoWidth: 'w-[80%]',
       }
     case 'web':
       return {
-        subtitle: '설치 가이드',
-        background: 'bg-[url(/cover/bg-sdk.svg)]',
-        clipBackground: 'bg-[url(/cover/bg-sdk-clip.svg)]',
+        background: 'bg-[url(/cover/bg-web-agent.svg)]',
         onycom: '/cover/onycom.svg',
         imqa: '/cover/imqa.svg',
         q: '/cover/q.svg',
         x: '/cover/x.svg',
-        clip: '/cover/web-agent-clip.svg',
-        clipOffset: '',
         typo: '/cover/web-agent-typo.svg',
-        typoWidth: 'w-[60%]',
+        typoWidth: 'w-[80%]',
       }
     case 'user-guide':
       return {
-        subtitle: '사용자 가이드',
         background: 'bg-[url(/cover/bg-user-guide.svg)]',
-        clipBackground: '',
         onycom: '/cover/onycom.svg',
         imqa: '/cover/imqa.svg',
         q: '/cover/q.svg',
         x: '/cover/x.svg',
-        clip: '/cover/user-guide-clip.svg',
-        clipOffset: 'top-[2%]',
         typo: '/cover/user-guide-typo.svg',
-        typoWidth: 'w-[45%]',
+        typoWidth: 'w-[80%]',
       }
     case 'admin-guide':
       return {
-        subtitle: '관리자 가이드',
         background: 'bg-[url(/cover/bg-admin-guide.svg)]',
-        clipBackground: 'bg-[url(/cover/bg-admin-guide-clip.svg)]',
         onycom: '/cover/onycom.svg',
         imqa: '/cover/imqa.svg',
         q: '/cover/q.svg',
         x: '/cover/x.svg',
-        clip: '/cover/admin-guide-clip.svg',
-        clipOffset: 'left-[14%]',
         typo: '/cover/admin-guide-typo.svg',
-        typoWidth: 'w-[50%]',
+        typoWidth: 'w-[80%]',
       }
   }
 }
 export function Cover({ kind }: CoverProps) {
-  const {
-    subtitle,
-    background,
-    clipBackground,
-    onycom,
-    imqa,
-    q,
-    x,
-    clip,
-    clipOffset,
-    typo,
-    typoWidth,
-  } = getAssets(kind)
+  const { background, onycom, imqa, q, x, typo, typoWidth } = getAssets(kind)
   return (
     <div className="flex min-h-screen w-full flex-row justify-center bg-transparent">
       <Card className="aspect-[1/1.414] w-full max-w-[1200px] border-0">
         <CardContent
           className={cn(['relative h-full w-full', background, 'bg-cover bg-center p-0'])}
         >
-          <div className="absolute bottom-[17%] left-[10%] whitespace-nowrap text-[clamp(2em,4vw,48px)] font-normal leading-[1.2] tracking-[0] text-[#8080ff]">
-            {subtitle}
-          </div>
-
-          <p className="absolute bottom-[12%] left-[10%] text-[clamp(0.8em,1.8vw,21.6px)] font-normal leading-[normal] tracking-[0] text-white">
+          <p className="absolute bottom-[9%] left-[6.5%] text-[clamp(0.8em,1.3vw,16px)] font-normal leading-[normal] tracking-[0] text-white">
             Copyright © 2024-{new Date().getFullYear()} by ONYCOM. Inc. All Rights Reserved.
           </p>
 
-          <div className="absolute bottom-[5%] left-[10%] w-[48%]">
-            <p className="text-[clamp(0.8em,1.8vw,21.6px)] font-normal leading-[1.5] tracking-[0] text-white">
+          <div className="absolute bottom-[4.8%] left-[6.5%] w-[50%]">
+            <p className="text-[clamp(0.8em,1.3vw,16px)] font-normal leading-[1.5] tracking-[0] text-white">
               본 문서는 어니컴(주)이 발행하는 문서이며 저작권법에 의해 보호를 받는 저작물이므로
               발행처의 허가 없이 무단 전재나 복제를 금합니다 .
             </p>
-
-            {/* <p className="mt-1 text-[1.8vh] font-normal leading-[1.5] tracking-[0] text-white">
-              보호를 받는 저작물이므로 발행처의 허가 없이 무단 전재나 복제를 금합니다 .
-            </p> */}
           </div>
 
           <Image
-            className={cn(['absolute bottom-[25%] left-[10%] h-auto', typoWidth])}
+            className={cn(['absolute left-[6.5%] top-[4.7%] h-auto', typoWidth])}
             alt="sdk-typo"
             src={typo}
-            width={800}
-            height={200}
+            width={970}
+            height={300}
             unoptimized
           />
 
-          <div className="absolute left-[10%] top-[6%] w-[23%]">
+          <div className="absolute bottom-[6%] right-[6.5%] w-[24%]">
             <div className="relative">
               <Image
                 className="absolute left-[44%] top-1 h-[10px] w-[10px]"
@@ -186,23 +142,6 @@ export function Cover({ kind }: CoverProps) {
                 </div>
               </div>
             </div>
-          </div>
-
-          <div
-            className={cn([
-              'absolute left-[10%] top-[9%] h-[55%] w-[80%]',
-              clipBackground,
-              'bg-cover bg-center',
-            ])}
-          >
-            <Image
-              className={cn(['absolute top-[3%] h-[96%] w-full', clipOffset])}
-              alt="clip"
-              src={clip}
-              width={1000}
-              height={600}
-              unoptimized
-            />
           </div>
         </CardContent>
       </Card>
